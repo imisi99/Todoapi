7c5ceeac49894bdbe367660fc14bfdcedc19adc8
imisi99 <isongrichard234@gmail.com>
2024-01-15 14:24:48 +0100

    Writing Code

---
+++ schemas/model_db.py
@@ -0,0 +1,24 @@
+from .database import data
+from sqlalchemy import Column, Integer, ForeignKey, Boolean, String, Float
+
+class User(data):
+
+    __tablename__ = "User"
+
+    id = Column(Integer, primary_key= True, index= True)
+    first_name = Column(String, nullable= False)
+    last_name = Column(String, nullable= False)
+    username = Column(String(50), unique= True, nullable= False)
+    email = Column(String, nullable= False)
+    password = Column(String, nullable=False)
+
+
+class Todo(data):
+
+    __tablename__ = "Todo"
+
+    id = Column(Integer, primary_key= True, index= True)
+    task = Column(String, nullable= False)
+    completed = Column(Boolean, nullable= False)
+    note = Column(String(50), nullable= True)
+    user_id = Column(Integer, ForeignKey("User.id"))
