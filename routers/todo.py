23ebbf07ffac6fa99114a18f7aec097481a89f58
imisi99 <isongrichard234@gmail.com>
2024-01-23 20:38:18 +0100

    Adding Validation and defining classes

--- routers/todo.py
+++ routers/todo.py
@@ -4,8 +4,10 @@ from schemas.model_db import Todo
 import schemas.model_db as model_db
 from starlette import status
 from pydantic import BaseModel, Field
-from typing import Annotated
+from typing import Annotated, Optional
 from .user import get_user
+from datetime import datetime
+
 
 todo = APIRouter()
 
@@ -23,5 +25,59 @@ def get_db():
 db_dependency = Annotated[str, Depends(get_db)]
 user_dependancy = Annotated[str, Depends(get_user)]
 
+#Todo create class
+class CreateTodo(BaseModel):
+    tasks : Annotated[str, Field(max_length= 100)]
+    completed : Annotated[bool, Field(default= False)]
+    note : Annotated[str, Field(max_length= 100)]
+    due : Annotated[datetime, Field]
+    
+    class Config():
+        json_schema_extra = {
+            'example' : {
+                "tasks" : "tasks",
+                "completed" : False,
+                "note" : "note",
+                "due" : ""
+            }
+        }
 
+#Update Todo details class
+class UpdateTodo():
+    tasks : Annotated[str, Field(max_length= 100)]
+    completed : Annotated[bool, Field(default= False)]
+    note : Annotated[str, Field(max_length= 100)]
+    due : Annotated[datetime, Field]
+    user_id : Annotated[int, Field(Optional)]
+
+    class Config():
+        json_schema_extra = {
+            'example' : {
+                "tasks" : "tasks",
+                "completed" : False,
+                "note" : "note",
+                "due" : ""
+            }
+        }
+
+#Update Todo via completed class
+class CompleteTodo():
+    completed : Annotated[bool, Field(default=True)]
+
+    class Config():
+        json_schema_extra = {
+            'example' : {
+                "completed" : True
+            }
+        }
+
+#get all tasks route
+#get tasks by id route
+#get tasks by title route
+#get tasks by completed route
+#create tasks route
+#update tasks route to be complete
+#update tasks route to update other details 
+#delete tasks route with tasks name 
+#delete tasks route with completed todo
 
--- routers/user.py
+++ routers/user.py
@@ -43,13 +43,94 @@ class UserForm(BaseModel):
     class Config():
         json_schema_extra = {
             'example' : {
-                "firstname" : "firstname",
-                "lastname" : "lastname",
-                "username" : "username",
+                "firstname" : "Firstname",
+                "lastname" : "Lastname",
+                "username" : "Username",
                 "gmail" : "email@gmail.com",
-                "password" : "passweord"
+                "password" : "Password"
             }
         }
 
+#Login form class
+class LoginForm(BaseModel):
+    username : Annotated[str, Field]
+    password : Annotated[str, Field]
 
-# User Authorization and Authentication 
+    class Config():
+        json_schema_extra = {
+            'example' : {
+                "username" : "Username",
+                "password" : "Password"
+            }
+        }
+
+#Update details class 
+class UpdateUser(BaseModel):
+    firstname : Annotated[str, Field(min_length= 3, max_length= 20)]
+    lastname : Annotated[str, Field(min_length=3, max_length= 20)]
+    username : Annotated[str, Field(min_length= 3, max_length= 15)]
+    email : Annotated[EmailStr, Field]
+
+    class Config():
+        json_schema_extra = {
+            'example' : {
+                "firstname" : "Firstname",
+                "lastname" : "Lastname",
+                "username" : "Username",
+                "email" : "email@gmail.com"
+            }
+        }
+
+#Change password class
+class NewPassword(BaseModel):
+    new_password : Annotated[str, Field(min_length= 8, description= "Password must conatain at least 8 character, one special character, and one Upper case letter")]
+    confirm_password : Annotated[str, Field()]
+
+    @validator("new_password")
+    def check_password(cls, value):
+        if len(value) < 8 :
+            raise ValueError("Password must be at least 8 characters long!.")
+        if not any(char.isupper() for char in value):
+            raise ValueError("Password must contain at leat one upper-case character!.")
+        if not re.search(r'[!@#$%^&*(),.?:{}|<>]', value):
+            raise ValueError("Password must contain at least one special character!.")
+        
+    class Config():
+        json_schema_extra = {
+            'example' : {
+                "new_password" : "new_password",
+                "confirm_password" : "confirm_password"
+            }
+        }
+#Forgotten user password 
+class ChangePassword(BaseModel):
+    username : Annotated[str, Field(min_length= 3, max_length= 15)]
+    new_password : Annotated[str, Field(min_length= 8, description= "Password must conatain at least 8 character, one special character, and one Upper case letter")]
+    confirm_password : Annotated[str, Field]
+
+    @validator("new_password")
+    def check_password(cls, value):
+        if len(value) < 8 :
+            raise ValueError("Password must be at least 8 characters long!.")
+        if not any(char.isupper() for char in value):
+            raise ValueError("Password must contain at least one upper-case character!.")
+        if not re.search(r'[!@#$%^&*(),.?:{}|<>]'):
+            raise ValueError("Password must contain at least one special character!.")
+        
+    class Config():
+        json_schema_extra = {
+            'example' : {
+                "username" : "Username",
+                "password" : "Password",
+                "confirm password" : " re-type Password"
+            }
+        }
+
+#User Authorization and Authentication 
+#User Singup route
+#User login route
+#User get details route
+#User update details route 
+#User change password route 
+#User forgot password route 
+#User delete route
