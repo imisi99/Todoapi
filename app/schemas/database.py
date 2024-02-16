b870c69c319b1e46842c669f2e51dd0b18627569
imisi99 <isongrichard234@gmail.com>
2024-02-16 13:03:22 +0100

    Changing the postgres url link

--- app/schemas/database.py
+++ app/schemas/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import declarative_base
 
-database = 'postgresql://postgres:Imisioluwa234.@localhost/Todoapi'
+database = 'postgresql://ffivcxms:OwG3mL4x_u-T9HLC3Q8Axn8mCCPTmBgP@trumpet.db.elephantsql.com/ffivcxms'
 engine = create_engine(database)
 begin = sessionmaker(bind = engine, autocommit = False, autoflush = False)
 data = declarative_base()
