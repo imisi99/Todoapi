d520e3727fc147fb700d4b8dd91063067950a807
imisi99 <isongrichard234@gmail.com>
2024-02-17 20:17:32 +0100

    Reversing the link

--- app/schemas/database.py
+++ app/schemas/database.py
@@ -2,8 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import declarative_base
 
-#  elephant database = 'postgresql://ffivcxms:OwG3mL4x_u-T9HLC3Q8Axn8mCCPTmBgP@trumpet.db.elephantsql.com/ffivcxms'
-database = 'postgresql://todoapi_k4cj_user:vp2FGRZF0TJuF18xiCVs1Vx3pH7pH6Cd@dpg-cn8fna7109ks739ol93g-a/todoapi_k4cj'
+database = 'postgresql://ffivcxms:OwG3mL4x_u-T9HLC3Q8Axn8mCCPTmBgP@trumpet.db.elephantsql.com/ffivcxms'
 engine = create_engine(database)
 begin = sessionmaker(bind = engine, autocommit = False, autoflush = False)
 data = declarative_base()
