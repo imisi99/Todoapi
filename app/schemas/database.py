dda26ddc081a764666e30ae03ccba1cc58895253
imisi99 <isongrichard234@gmail.com>
2024-02-17 19:52:45 +0100

    Using render postgresql

--- app/schemas/database.py
+++ app/schemas/database.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import declarative_base
 
-database = 'postgresql://ffivcxms:OwG3mL4x_u-T9HLC3Q8Axn8mCCPTmBgP@trumpet.db.elephantsql.com/ffivcxms'
+#  elephant database = 'postgresql://ffivcxms:OwG3mL4x_u-T9HLC3Q8Axn8mCCPTmBgP@trumpet.db.elephantsql.com/ffivcxms'
+database = 'postgresql://todoapi_k4cj_user:vp2FGRZF0TJuF18xiCVs1Vx3pH7pH6Cd@dpg-cn8fna7109ks739ol93g-a/todoapi_k4cj'
 engine = create_engine(database)
 begin = sessionmaker(bind = engine, autocommit = False, autoflush = False)
 data = declarative_base()
